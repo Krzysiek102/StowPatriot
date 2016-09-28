8eecd9e63acfb909b88811c8e297edc70b519afa
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-09-28 19:58:16 +0200

    grunt watch css

--- gruntfile.js
+++ gruntfile.js
@@ -1,55 +1,65 @@
-module.exports = function (grunt) {  
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);  
+module.exports = function (grunt) {
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
     // Project configuration.  
-    grunt.initConfig({  
-        pkg: grunt.file.readJSON('package.json'),  
-        cssmin: {  
-            sitecss: {  
-                files: {  
-                    'bundles/styles.min.css': [  
-                        'bower_components/pure/pure-min.css',  
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        cssmin: {
+            sitecss: {
+                files: {
+                    'bundles/styles.min.css': [
+                        'bower_components/pure/pure-min.css',
                         'bower_components/pure/grids-responsive-min.css',
                         'styles/blog.css',
-                        'styles/app.css']  
-                }  
-            }  
-        },  
-        uglify: {  
-            options: {  
-                compress: false  
-            },  
-            applib: {  
-                src: [  
-                'bower_components/angular/angular.min.js',
-                'bower_components/angular-route/angular-route.min.js',
-                'scripts/domain/consts.js',
-                'scripts/domain/post.js',
-                'scripts/domain/spPosts.js',
-                'scripts/domain/spMenu.js',
-                'scripts/domain/postController.js',
-                'scripts/domain/postsController.js',
-                'scripts/domain/routingDefinition.js',
-                'scripts/domain/app.js'
-                ],  
-                dest: 'bundles/scripts.min.js'  
-            }  
+                        'styles/app.css']
+                }
+            }
+        },
+        uglify: {
+            options: {
+                compress: false
+            },
+            applib: {
+                src: [
+                    'bower_components/angular/angular.min.js',
+                    'bower_components/angular-route/angular-route.min.js',
+                    'scripts/domain/consts.js',
+                    'scripts/domain/post.js',
+                    'scripts/domain/spPosts.js',
+                    'scripts/domain/spMenu.js',
+                    'scripts/domain/postController.js',
+                    'scripts/domain/postsController.js',
+                    'scripts/domain/routingDefinition.js',
+                    'scripts/domain/app.js'
+                ],
+                dest: 'bundles/scripts.min.js'
+            }
         },
         watch: {
-            files: [  
-                'bower_components/angular/angular.min.js',
-                'bower_components/angular-route/angular-route.min.js',
-                'scripts/domain/consts.js',
-                'scripts/domain/post.js',
-                'scripts/domain/spPosts.js',
-                'scripts/domain/spMenu.js',
-                'scripts/domain/postController.js',
-                'scripts/domain/postsController.js',
-                'scripts/domain/routingDefinition.js',
-                'scripts/domain/app.js'
+            js: {
+                files: [
+                    'bower_components/angular/angular.min.js',
+                    'bower_components/angular-route/angular-route.min.js',
+                    'scripts/domain/consts.js',
+                    'scripts/domain/post.js',
+                    'scripts/domain/spPosts.js',
+                    'scripts/domain/spMenu.js',
+                    'scripts/domain/postController.js',
+                    'scripts/domain/postsController.js',
+                    'scripts/domain/routingDefinition.js',
+                    'scripts/domain/app.js'
                 ],
-            tasks: ['uglify']
+                tasks: ['uglify']
+            },
+            css: {
+                files: [
+                    'bower_components/pure/pure-min.css',
+                    'bower_components/pure/grids-responsive-min.css',
+                    'styles/blog.css',
+                    'styles/app.css'],
+                tasks: ['cssmin']
+            }
         }
-    });  
+    });
     // Default task.  
-    grunt.registerTask('default', ['uglify', 'cssmin']);  
+    grunt.registerTask('default', ['uglify', 'cssmin']);
 };
