279fe66c8f72106151061ccd7667d8c9c1e3ce44
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-10-05 20:29:38 +0200

    integration tets for filtering and sorting

--- tests/e2e/newsSpecs.js
+++ tests/e2e/newsSpecs.js
@@ -1,6 +1,27 @@
-describe('News page tests', function() {
-  it('should have a title', function() {
+describe('News page tests', function () {
+  it('should have a title', function () {
     browser.get('http://localhost:8000/#/news');
     expect(browser.getTitle()).toEqual("Stowarzyszenie Chrześcijańsko - Patrytiotyczne Ziemi Włodawskiej");
   });
+
+  it('should filter news list', function () {
+    browser.get('http://localhost:8000/#/news');
+
+    element(by.model('query')).sendKeys('w obronie wolnych mediów');
+
+    var news = element.all(by.repeater('post in c.posts'));
+    expect(news.count()).toEqual(1);
+  });
+
+  it('should sort news list', function () {
+    browser.get('http://localhost:8000/#/news');
+
+    element(by.model('c.orderMode')).sendKeys('date');
+
+    var news = element.all(by.repeater('post in c.posts'));
+
+    var oldest = news[0].element(by.className('post-meta'));
+    expect(oldest.getText()).toEqual('2013-06-19');
+  });
+
 });
