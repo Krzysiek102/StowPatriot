2dd49a50485cf39e579d15ae35965f6a54488c13
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-10-21 08:59:22 +0200

    More integration tests against news to cover posts scenarios

--- tests/e2e/newsSpecs.js
+++ tests/e2e/newsSpecs.js
@@ -16,6 +16,9 @@ describe('News page tests', function () {
     element(by.model('query')).sendKeys('w obronie wolnych mediów');
     var news = element.all(by.repeater('post in c.posts'));
     expect(news.count()).toEqual(1);
+    var soleItem = news.first();
+    expect(soleItem.element(by.css('.post-title')).getText()).toBe('Marsz w obronie wolnych mediów we Włodawie');
+    expect(soleItem.element(by.css('.post-description')).getText()).toBe('Marsz w obronie wolnych mediów {telewizji Trwam i Radia Maryja} odbył się we Włodawie dn. 19 06 2013r.W marszu obok mieszkańców Włodawy wzieli udział parlamentarzyści oraz przedstawiciele władz miasta i powiatu a swoją obecnością uświetniła marsz "kapela z nad Baryczy"');
 
     //clear the filtering
     element(by.model('query')).clear().then(function () {
@@ -46,9 +49,9 @@ describe('News page tests', function () {
   it('should navigate to detailed page after clicking the title and return after clicking return button', function () {
     var news = element.all(by.repeater('post in c.posts'));
     news.last().element(by.css('.post-title-link')).click().then(function () {
-      var newsTitle = element(by.css('.post-title')).getText();
       //we redirected to the correct news
-      expect(newsTitle).toBe('Marsz w obronie wolnych mediów we Włodawie');
+      expect(element(by.css('.post-title')).getText()).toBe('Marsz w obronie wolnych mediów we Włodawie');
+      expect(element(by.css('.post-meta')).getText()).toBe('2013-06-19');
       var returnButton = element(by.css('#return-button'));
       returnButton.click().then(function () {
         //we have more then one news one page
@@ -57,5 +60,33 @@ describe('News page tests', function () {
       })
     })
   });
+
+  it('should navigate to detailed page after clicking more info button and return after clicking return button', function () {
+    var news = element.all(by.repeater('post in c.posts'));
+    news.last().element(by.css('.post-more-info')).click().then(function () {
+      //we redirected to the correct news
+      expect(element(by.css('.post-title')).getText()).toBe('Marsz w obronie wolnych mediów we Włodawie');
+      var returnButton = element(by.css('#return-button'));
+      returnButton.click().then(function () {
+        //we have more then one news one page
+        news = element.all(by.repeater('post in c.posts'));
+        expect(news.count()).toBeGreaterThan(1);
+      })
+    })
+  });  
+
+  it('should navigate to detailed page after clicking image and return after clicking return button', function () {
+    var news = element.all(by.repeater('post in c.posts'));
+    news.last().element(by.css('.post-avatar')).click().then(function () {
+      //we redirected to the correct news
+      expect(element(by.css('.post-title')).getText()).toBe('Marsz w obronie wolnych mediów we Włodawie');
+      var returnButton = element(by.css('#return-button'));
+      returnButton.click().then(function () {
+        //we have more then one news one page
+        news = element.all(by.repeater('post in c.posts'));
+        expect(news.count()).toBeGreaterThan(1);
+      })
+    })
+  });   
 
 });
