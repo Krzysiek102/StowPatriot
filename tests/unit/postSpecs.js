b59cdfb36f04901b8be9625e33227472eca73e3f
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2016-10-21 08:36:23 +0200

    Unit test infrastructure

---
+++ tests/unit/postSpecs.js
@@ -0,0 +1,11 @@
+/// <reference path="../../typings/globals/jasmine/index.d.ts" />
+
+
+describe('Post', function() {
+    
+    it('test', function() {
+        expect(1).toBe(1);
+    });
+        
+});
+    
